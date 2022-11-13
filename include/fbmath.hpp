0b9646e8dae3b262a38735f4f985fcd4d7c255c1
Felix Berg <felixbpedersen@gmail.com>
2022-11-13 22:13:04 +0100

    include subpath

--- include/fbmath.hpp
+++ include/fbmath.hpp
@@ -1,11 +1,11 @@
 // Created by felix on 11/13/22, 7:26 PM.
 #pragma once
 
-#include <2D/vec2.hpp>
-#include <2D/ray.hpp>
-#include <2D/boundingbox.hpp>
-#include <2D/circle.hpp>
-#include <2D/circle-ray-intersection.hpp>
-#include <3D/vec3.hpp>
-#include <ostream_interop.hpp>
-#include <trigonometry.hpp>
+#include <fbmath/2D/vec2.hpp>
+#include <fbmath/2D/ray.hpp>
+#include <fbmath/2D/boundingbox.hpp>
+#include <fbmath/2D/circle.hpp>
+#include <fbmath/2D/circle-ray-intersection.hpp>
+#include <fbmath/3D/vec3.hpp>
+#include <fbmath/ostream_interop.hpp>
+#include <fbmath/trigonometry.hpp>
