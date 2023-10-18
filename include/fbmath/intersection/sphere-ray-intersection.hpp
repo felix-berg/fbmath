1436f089a22fa9f75145db6f740608962d5e1355
felix berg <felixbpedersen@gmail.com>
2023-10-19 00:01:10 +0200

    extracted part of sphere-ray-intersection

--- include/fbmath/intersection/sphere-ray-intersection.hpp
+++ include/fbmath/intersection/sphere-ray-intersection.hpp
@@ -10,36 +10,53 @@ template <typename T>
 using SphereRayIntersection =
     std::optional<std::pair<Vec3<T>, Vec3<T>>>;
 
-template <Number S, Number R, std::floating_point ResultType = MorePreciseType<S, R>>
-constexpr SphereRayIntersection<ResultType> intersection(
+template <Number S, Number R, std::floating_point T = MorePreciseType<S, R>>
+constexpr std::optional<std::pair<T, T>> sphereRayIntersectionParams(
     const Sphere<S>& sphere,
     const Ray<Vec3<R>>& ray
 ) noexcept
 {
-    using T = ResultType;
     using VS = Vec3<T>;
 
     const VS org = VS::from(ray.org);
     const VS oc = org - VS::from(sphere.c);
-    const VS dir = static_cast<VS>(ray.dir);
+    const VS dir = VS::from(ray.dir);
     const T rsq = static_cast<T>(sphere.r) * static_cast<T>(sphere.r);
 
     const T a = dir.sizeSquared();
     const T b = T{2} * (dotProduct(dir, oc));
     const T c = oc.sizeSquared() - rsq;
 
-    // discriminant
     const T dis = b * b - T{4} * a * c;
-    if (dis < T { 0 }) {
-        return std::nullopt;
-    } else if (dis == 0) {
-        VS its = org + dir * (-b / (T{2} * a));
-        return std::make_pair(its, its);
+    if (dis < T{0}) return std::nullopt;
+    else if (dis == 0) {
+        T value = -b / (T{2} * a);
+        return std::make_pair(value, value);
+    } else {
+        T sd = std::sqrt(dis);
+        T a2 = T{2} * a;
+        return std::make_pair(
+            (-b + sd) / a2,
+            (-b - sd) / a2
+        );
     }
+}
 
+template <Number S, Number R, std::floating_point ResultType = MorePreciseType<S, R>>
+constexpr SphereRayIntersection<ResultType> intersection(
+    const Sphere<S>& sphere,
+    const Ray<Vec3<R>>& ray
+) noexcept
+{
+    const auto param = sphereRayIntersectionParams<
+        S, R, ResultType>(sphere, ray);
+    if (!param) return std::nullopt;
+
+    Vec3<ResultType> o = static_cast<Vec3<ResultType>>(ray.org);
+    Vec3<ResultType> d = static_cast<Vec3<ResultType>>(ray.dir);
     return std::make_pair(
-        org + dir * (-b + std::sqrt(dis)) / (T{2} * a),
-        org + dir * (-b - std::sqrt(dis)) / (T{2} * a)
+          o + d * param->first,
+          o + d * param->second
     );
 }
 
