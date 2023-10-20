0bf1afc32dafcfe57ec8cedc71cab53c140e55de
felix berg <felixbpedersen@gmail.com>
2023-10-20 11:01:39 +0200

    include stuff

--- include/fbmath.hpp
+++ include/fbmath.hpp
@@ -2,11 +2,13 @@
 #pragma once
 
 #include "fbmath/vec2.hpp"
+#include "fbmath/vec3.hpp"
 #include "fbmath/ray.hpp"
 #include "fbmath/boundingbox.hpp"
 #include "fbmath/circle.hpp"
 #include "fbmath/intersection/circle-ray-intersection.hpp"
-#include "fbmath/vec3.hpp"
+#include "fbmath/sphere.hpp"
+#include "fbmath/intersection/sphere-ray-intersection.hpp"
 #include <fbmath/ostream_interop.hpp>
 #include <fbmath/trigonometry.hpp>
 #include <fbmath/valueutil.hpp>
